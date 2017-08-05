2520b4fbff08497cfe4adf649a7f7a6ff19cbb7a
Beatriz López <be.lop.mar@gmail.com>
2017-08-05 17:31:23 +0200

    Changes in Readme file and CSS styles

--- card/src/App.js
+++ card/src/App.js
@@ -21,9 +21,9 @@ class App extends Component {
             />
 
         <Card
-          name="Mr Menganito"
-          imageAlt="Foto de perfil de Mr Fulanito"
           image={avatar2}
+          imageAlt="Foto de perfil de Mr Fulanito"
+          name="Mr Menganito"
           date="2/07/2017"
           text="Boggarts lavender robes, Hermione Granger Fantastic Beasts and Where to Find Them. Bee in your bonnet Hand of Glory elder wand, spectacles House Cup Bertie Bott’s Every Flavor Beans Impedimenta. Stunning spells tap-dancing spider Slytherin’s Heir mewing kittens Remus Lupin. Palominos scarlet train black robes, Metamorphimagus Niffler dead easy second bedroom. Padma and Parvati Sorting Hat Minister of Magic blue turban remember my last."
           like={like}
